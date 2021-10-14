827919486b2ccd3974573eaa37bb8988e50e1742
Roberto Becerril <xdroberto36@gmail.com>
2021-10-13 19:15:56 -0500

    Actividad 12

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
 
@@ -16,6 +17,29 @@ user.post("/", async (req, res, next) => {
         return res.status(500).json({code:500, message:"Ocurrió un error"});
     }
     return res.status(500).json({code:500, message:"Campos incompletos"});
+});
+
+user.post("/login", async (req, res, next) =>{
+    const {user_mail, user_password} = req.body;
+    const query = `SELECT * FROM user WHERE user_mail = '${user_mail}' AND user_password = '${user_password}';`;
+    const rows = await db.query(query);
+    
+    if(user_mail && user_password){
+
+        if(rows.length == 1){
+            const token = jwt.sign({
+                user_id: rows[0].user_id,
+                user_mail: rows[0].user_mail
+            },"debugkey");
+            return res.status(200).json({code: 200, message: token});
+        }
+        else{
+            res.status(401).json({code:401, message: "Usuario y/o contraseña incorrectos"});
+    
+        }
+    }
+    return res.status(500).json({code:500, message:"Campos incompletos"});
+    
 });
 
 user.get("/", async (req, res, next) =>{

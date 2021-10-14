b3e22e814c3b00d3c91938350ac992a717f215c1
Roberto Becerril <xdroberto36@gmail.com>
2021-10-13 23:20:03 -0500

    Actividad 13

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+//Dependencias
 const morgan = require('morgan');
 const express= require('express');
 const app = express();
+//Routers
 const pokemon = require('./routes/pokemon');
 const user = require('./routes/user')
+//Middleware
+const auth= require('./middleware/auth');
+const notFound = require('./middleware/notFound')
+const index = require('./middleware/index')
 
 /*
 Verbos http
@@ -20,17 +26,12 @@ app.use(express.urlencoded({extended: true}));
 
 
 
-app.get("/", (req, res, next) => {
-    res.status(200).json({code: 1, message: "Bienvenido al pokedex"});
-});
+app.get("/",index);
 
-app.use("/pokemon", pokemon);
 app.use("/user/", user);
-
-
-app.use((req, res, next) => {
-    return res.status(404).json({code: 404, message: "URL no encontrada"});
-});
+app.use(auth);
+app.use("/pokemon", pokemon);
+app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...')

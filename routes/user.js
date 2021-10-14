179cc11b76d8cc213994a95fb26a202e67d8cfab
Roberto Becerril <xdroberto36@gmail.com>
2021-10-14 00:37:58 -0500

    Actividad 15

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const user = express.Router();
 const db = require('../config/database');
 
-user.post("/singin", async (req, res, next) => {
+user.post("/signin", async (req, res, next) => {
     const {user_name, user_mail, user_password} = req.body;
 
     if(user_name && user_mail && user_password){
@@ -34,7 +34,7 @@ user.post("/login", async (req, res, next) =>{
             return res.status(200).json({code: 200, message: token});
         }
         else{
-            res.status(200).json({code:200, message: "Usuario y/o contraseña incorrectos"});
+            res.status(200).json({code:401, message: "Usuario y/o contraseña incorrectos"});
     
         }
     }
